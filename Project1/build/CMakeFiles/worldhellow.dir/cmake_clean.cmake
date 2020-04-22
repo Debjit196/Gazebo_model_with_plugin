e1f017ede80c01c8c65e1c1a42ed6b7dfa9a635d
Debjit196 <41549881+Debjit196@users.noreply.github.com>
2020-04-22 16:19:12 +0000

    Add files via upload

---
+++ Project1/build/CMakeFiles/worldhellow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/worldhellow.dir/script/worldhellow.cpp.o"
+  "libworldhellow.pdb"
+  "libworldhellow.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/worldhellow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
